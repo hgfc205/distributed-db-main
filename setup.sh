d2e7e68a511fce64df65d07ba9a291300e715451
Hektor Germain Flores Cabrera <hektorgermain@gmail.com>
2024-05-20 20:28:00 -0700

    Script de setup XD

---
+++ setup.sh
@@ -0,0 +1,3 @@
+# Al runnear la PC, runnea este script
+systemctl start mariadb
+nmcli connection up enp0s3
--- webapp/src/app/environment/env.ts
+++ webapp/src/app/environment/env.ts
@@ -1,1 +1,1 @@
-export const addr = 'http://127.0.0.1:5000'
+export const addr = 'http://192.168.1.107:5000'
